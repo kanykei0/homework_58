929db028b59653997c653b6a0e926d03556a228f
Duishenalieva <liltril783@gmail.com>
2023-11-14 17:30:29 +0600

    minor fixes and created clickDismissable

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import Alert from "./components/Alert/Alert";
 function App() {
   const [showModal, setShowModal] = useState(false);
   const [showModalMessage, setShowModalMessage] = useState(false);
-  const [showAlert, setShowAlert] = useState(true);
+  const [success, setSuccess] = useState(true);
+  const [danger, setDanger] = useState(true);
+  const [warning, setWarning] = useState(false);
 
   const continueBtn = () => {
     setShowModal(false);
     setShowModalMessage(true);
   };
 
-  const openAlert = () => {
+  const openModalMessage = () => {
     return (
       <Modal
         show={true}
@@ -35,29 +37,48 @@ function App() {
 
   return (
     <>
-      <button className="btn btn-success" onClick={() => setShowModal(true)}>
-        Modal
-      </button>
-      <Modal
-        show={showModal}
-        title="Modal head!"
-        onClose={() => setShowModal(false)}
-        buttons={modalButtons}
-      >
-        <div className="modal-body">Content</div>
-      </Modal>
-      {showModalMessage ? openAlert() : null}
-      <Alert
-        show={showAlert}
-        type="success"
-        onDismiss={() => setShowAlert(false)}
-      >
-        Hello World!
-      </Alert>
+      <div className="container-md">
+        <button
+          className="btn btn-success my-3"
+          onClick={() => setShowModal(true)}
+        >
+          Modal
+        </button>
+        <Modal
+          show={showModal}
+          title="Modal head!"
+          onClose={() => setShowModal(false)}
+          buttons={modalButtons}
+        >
+          <div className="modal-body">Content</div>
+        </Modal>
+        {showModalMessage ? openModalMessage() : null}
+        <Alert
+          show={success}
+          type="success"
+          onDismiss={() => setSuccess(false)}
+        >
+          Hello World!
+        </Alert>
 
-      <Alert show={true} type="danger">
-        Bye World!
-      </Alert>
+        <Alert
+          show={danger}
+          type="danger"
+          clickDismissable
+          onDismiss={() => setDanger(false)}
+        >
+          Bye World!
+        </Alert>
+        <button
+          className="btn btn-primary mb-3 d-block"
+          onClick={() => setWarning(!warning)}
+        >
+          {warning ? "Hide warning alert" : "Show warning alert"}
+        </button>
+        <Alert show={warning} type="warning">
+          Bye World!
+        </Alert>
+      </div>
     </>
   );
 }
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -4,15 +4,25 @@ interface Props extends React.PropsWithChildren {
   show: boolean;
   type: string;
   onDismiss?: React.MouseEventHandler;
+  clickDismissable?: boolean;
 }
 
-const Alert: React.FC<Props> = ({ show, type, onDismiss, children }) => {
+const Alert: React.FC<Props> = ({
+  show,
+  type,
+  onDismiss,
+  clickDismissable,
+  children,
+}) => {
   {
     return (
-      <div style={{ display: show ? "block" : "none" }}>
+      <div
+        style={{ display: show ? "block" : "none" }}
+        onClick={clickDismissable ? onDismiss : undefined}
+      >
         <div className={`alert alert-${type} col-6 row`}>
           <div className="col">{children}</div>
-          {onDismiss ? (
+          {onDismiss && !clickDismissable ? (
             <button className="btn-close col-1" onClick={onDismiss} />
           ) : null}
         </div>
